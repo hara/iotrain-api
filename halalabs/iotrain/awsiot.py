fee6b556ace74289a871f927377c35456babaaec
Yoshihiro Hara <hara.yoshihiro@gmail.com>
2019-02-12 23:50:41 +0900

    refactor: inject awsiot client

--- halalabs/iotrain/awsiot.py
+++ halalabs/iotrain/awsiot.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+from AWSIoTPythonSDK.core.shadow.deviceShadow import deviceShadow
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 
 from halalabs.iotrain import gateways, usecases, utils
@@ -9,42 +10,18 @@ from halalabs.iotrain.controllers import DriveController
 from halalabs.iotrain.entities import Drive
 
 
-class IoTApp:
-    def __init__(self,
-                 thing_name: str,
-                 endpoint_host: str,
-                 endpoint_port: int,
-                 root_ca_path: str,
-                 private_key_path: str,
-                 certificate_path: str,
-                 connect_disconnect_timeout=10,
-                 mqtt_operation_timeout=5):
-        self.thing_name = thing_name
-        self.client = AWSIoTMQTTShadowClient(thing_name)
-        self.client.configureEndpoint(endpoint_host, endpoint_port)
-        self.client.configureCredentials(root_ca_path, private_key_path,
-                                         certificate_path)
-        self.client.configureConnectDisconnectTimeout(
-            connect_disconnect_timeout)
-        self.client.configureMQTTOperationTimeout(mqtt_operation_timeout)
-
-    def _delta_name(self):
-        return 'delta/{thing_name}'.format(thing_name=self.thing_name)
+class App:
+    def __init__(self, shadow: deviceShadow):
+        self.shadow = shadow
 
     @utils.logging
     def _delta_callback(self, payload, response_status, token):
-        if response_status == self._delta_name():
-            payload_dict = json.loads(payload)
-            if payload_dict['state'].get('drive'):
-                self.controller.operate(payload_dict['state']['drive'])
-        else:
-            self.logger.error('responseStatus', response_status)
+        payload_dict = json.loads(payload)
+        if payload_dict['state'].get('drive'):
+            self.controller.operate(payload_dict['state']['drive'])
 
     @utils.logging
     def start(self):
-        self.client.connect()
-        self.shadow = self.client.createShadowHandlerWithName(
-            self.thing_name, True)
         drive = Drive()
         drive_context = DriveContext(drive)
         motor_gateway = gateways.MotorGateway()
@@ -63,7 +40,6 @@ class IoTApp:
     @utils.logging
     def stop(self):
         self.shadow.shadowUnregisterDeltaCallback()
-        self.client.disconnect()
 
 
 def app():
@@ -73,11 +49,20 @@ def app():
     private_key_path = os.environ['PRIVATE_KEY_PATH']
     certificate_path = os.environ['CERTIFICATE_PATH']
 
-    iot_app = IoTApp(thing_name, endpoint_host, 8883, root_ca_path,
-                     private_key_path, certificate_path)
-    iot_app.start()
+    client = AWSIoTMQTTShadowClient(thing_name)
+    client.configureEndpoint(endpoint_host, 8883)
+    client.configureCredentials(root_ca_path, private_key_path,
+                                certificate_path)
+    client.configureConnectDisconnectTimeout(10)
+    client.configureMQTTOperationTimeout(5)
+
+    client.connect()
     try:
+        shadow = client.createShadowHandlerWithName(thing_name, True)
+        app = App(shadow)
+        app.start()
         while True:
             pass
     except KeyboardInterrupt:
-        iot_app.stop()
+        app.stop()
+        client.disconnect()
