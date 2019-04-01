ad295bfbb6af7c5546fc0d43a3ee64cd4d3671e3
Yoshihiro Hara <hara.yoshihiro@gmail.com>
2019-04-01 23:15:29 +0900

    refactor

--- halalabs/iotrain/usecases.py
+++ halalabs/iotrain/usecases.py
@@ -47,31 +47,8 @@ class IShadowGateway(ABC):
         pass
 
 
-class IDriveStartInputPort(ABC):
-    @abstractmethod
-    def execute(self):
-        pass
-
-
-class DriveStartOutputData:
-    def __init__(self, direction: Direction, speed: Speed):
-        self.direction = direction
-        self.speed = speed
-
-
-class DriveStartInteractor(IDriveStartInputPort):
-    def __init__(self, context: IDriveContext, shadow_gateway: IShadowGateway):
-        self.context = context
-        self.shadow_gateway = shadow_gateway
-
-    @utils.logging
-    def execute(self):
-        self.shadow_gateway.update(self.context.drive.current_state.direction,
-                                   self.context.drive.current_state.speed)
-
-
 class DriveOperateInputData(InputData):
-    def __init__(self, direction: Direction = None, speed: Speed = None):
+    def __init__(self, direction: Direction, speed: Speed):
         self.direction = direction
         self.speed = speed
 
@@ -87,21 +64,16 @@ class DriveOperateInputData(InputData):
         invalid = InvalidInputData()
 
         direction = None
-        if dict_.get('direction'):
-            if dict_['direction'] in [d.name for d in Direction]:
-                direction = Direction[dict_['direction']]
-            else:
-                invalid.add_error(
-                    'direction must be STOP or FORWARD or BACKWARD')
-        speed = None
-        if dict_.get('speed'):
-            if isinstance(dict_['speed'], int) and 0 <= dict_['speed'] <= 100:
-                speed = Speed(dict_['speed'])
-            else:
-                invalid.add_error('speed must be integer between 0 and 100')
+        if dict_.get('direction') in [d.name for d in Direction]:
+            direction = Direction[dict_['direction']]
+        else:
+            invalid.add_error('direction must be STOP or FORWARD or BACKWARD')
 
-        if direction is None and speed is None:
-            invalid.add_error('direction or speed must be specified')
+        speed = None
+        if isinstance(dict_.get('speed'), int) and 0 <= dict_['speed'] <= 100:
+            speed = Speed(dict_['speed'])
+        else:
+            invalid.add_error('speed must be integer between 0 and 100')
 
         if invalid.has_errors():
             return invalid
@@ -127,8 +99,8 @@ class DriveOperateInteractor(IDriveOperateInputPort):
         if not input:
             raise exceptions.DriveOperationError
 
-        if self.context.drive.operate(input.direction, input.speed):
-            self.motor_gateway.control(
-                self.context.drive.current_state.direction,
-                self.context.drive.current_state.speed)
-            self.shadow_gateway.update(input.direction, input.speed)
+        self.context.drive.operate(input.direction, input.speed)
+        self.motor_gateway.control(self.context.drive.direction,
+                                   self.context.drive.speed)
+        self.shadow_gateway.update(self.context.drive.direction,
+                                   self.context.drive.speed)
--- tests/test_usecases.py
+++ tests/test_usecases.py
@@ -42,46 +42,35 @@ class TestInvalidInputData:
 
 class TestDriveOperateInputData:
     def test_from_dict(self):
-        input_dict = {'direction': 'FORWARD'}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        assert input_data.direction == usecases.Direction.FORWARD
-        assert input_data.speed is None
-
-        input_dict = {'speed': 10}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        assert input_data.direction is None
-        assert input_data.speed == usecases.Speed(10)
-
         input_dict = {'direction': 'FORWARD', 'speed': 10}
         input_data = usecases.DriveOperateInputData.from_dict(input_dict)
         assert input_data.direction == usecases.Direction.FORWARD
         assert input_data.speed == usecases.Speed(10)
 
     def test_invalid_direction(self):
-        input_dict = {'direction': 'INVALID'}
+        input_dict = {'direction': 'INVALID', 'speed': 10}
         input_data = usecases.DriveOperateInputData.from_dict(input_dict)
         assert not input_data
         assert input_data.errors[
             0] == 'direction must be STOP or FORWARD or BACKWARD'
 
     def test_invalid_speed(self):
-        input_dict = {'speed': '1'}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        assert not input_data
-        assert input_data.errors[
-            0] == 'speed must be integer between 0 and 100'
+        error = 'speed must be integer between 0 and 100'
 
-        input_dict = {'speed': 101}
+        input_dict = {'direction': 'FORWARD', 'speed': -1}
         input_data = usecases.DriveOperateInputData.from_dict(input_dict)
         assert not input_data
-        assert input_data.errors[
-            0] == 'speed must be integer between 0 and 100'
+        assert input_data.errors[0] == error
 
-    def test_no_operation(self):
-        input_dict = {}
+        input_dict = {'direction': 'FORWARD', 'speed': 101}
         input_data = usecases.DriveOperateInputData.from_dict(input_dict)
         assert not input_data
-        assert input_data.errors[0] == 'direction or speed must be specified'
+        assert input_data.errors[0] == error
+
+        input_dict = {'direction': 'FORWARD', 'speed': '1'}
+        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
+        assert not input_data
+        assert input_data.errors[0] == error
 
 
 class TestDriveOperateInteractor:
@@ -96,33 +85,25 @@ class TestDriveOperateInteractor:
             assert operate.call_args[0] == (input_data.direction,
                                             input_data.speed)
 
-        input_dict = {'direction': 'BACKWARD'}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        with patch.object(drive_context.drive, 'operate') as operate:
-            interactor.execute(input_data)
-            assert operate.call_args[0] == (input_data.direction, None)
-
-        input_dict = {'speed': 20}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        with patch.object(drive_context.drive, 'operate') as operate:
-            interactor.execute(input_data)
-            assert operate.call_args[0] == (None, input_data.speed)
-
-    def test_not_operate(self, drive_context, motor_gateway, shadow_gateway):
+    def test_operation_error(self, drive_context, motor_gateway,
+                             shadow_gateway):
         interactor = usecases.DriveOperateInteractor(
             drive_context, motor_gateway, shadow_gateway)
 
-        input_dict = {'direction': 'FORWARD', 'speed': 10}
+        input_dict = {'direction': 'FORWARD'}
         input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        with patch.object(drive_context.drive, 'operate') as operate:
+        with pytest.raises(exceptions.DriveOperationError):
             interactor.execute(input_data)
-            assert len(operate.call_args_list) == 1
 
-        input_dict = {'direction': 'FORWARD', 'speed': 10}
+        input_dict = {'speed': 10}
         input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        with patch.object(drive_context.drive, 'operate') as operate:
+        with pytest.raises(exceptions.DriveOperationError):
             interactor.execute(input_data)
-            assert len(operate.call_args_list) == 1
+
+        input_dict = {}
+        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
+        with pytest.raises(exceptions.DriveOperationError):
+            interactor.execute(input_data)
 
     def test_control_motor(self, drive_context, motor_gateway, shadow_gateway):
         interactor = usecases.DriveOperateInteractor(
@@ -134,33 +115,6 @@ class TestDriveOperateInteractor:
         assert motor_gateway.control.call_args[0] == (input_data.direction,
                                                       input_data.speed)
 
-        input_dict = {'direction': 'BACKWARD'}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        interactor.execute(input_data)
-        assert motor_gateway.control.call_args[0] == (
-            input_data.direction, drive_context.drive.current_state.speed)
-
-        input_dict = {'speed': 20}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        interactor.execute(input_data)
-        assert motor_gateway.control.call_args[0] == (
-            drive_context.drive.current_state.direction, input_data.speed)
-
-    def test_not_control_motor(self, drive_context, motor_gateway,
-                               shadow_gateway):
-        interactor = usecases.DriveOperateInteractor(
-            drive_context, motor_gateway, shadow_gateway)
-
-        input_dict = {'direction': 'FORWARD', 'speed': 10}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        interactor.execute(input_data)
-        assert len(motor_gateway.control.call_args_list) == 1
-
-        input_dict = {'direction': 'FORWARD', 'speed': 10}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        interactor.execute(input_data)
-        assert len(motor_gateway.control.call_args_list) == 1
-
     def test_update_shadow(self, drive_context, motor_gateway, shadow_gateway):
         interactor = usecases.DriveOperateInteractor(
             drive_context, motor_gateway, shadow_gateway)
@@ -170,37 +124,3 @@ class TestDriveOperateInteractor:
         interactor.execute(input_data)
         assert shadow_gateway.update.call_args[0] == (input_data.direction,
                                                       input_data.speed)
-
-        input_dict = {'direction': 'BACKWARD'}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        interactor.execute(input_data)
-        assert shadow_gateway.update.call_args[0] == (input_data.direction,
-                                                      None)
-
-        input_dict = {'speed': 20}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        interactor.execute(input_data)
-        assert shadow_gateway.update.call_args[0] == (None, input_data.speed)
-
-    def test_not_update_shadow(self, drive_context, motor_gateway,
-                               shadow_gateway):
-        interactor = usecases.DriveOperateInteractor(
-            drive_context, motor_gateway, shadow_gateway)
-
-        input_dict = {'direction': 'FORWARD', 'speed': 10}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        interactor.execute(input_data)
-        assert len(shadow_gateway.update.call_args_list) == 1
-
-        input_dict = {'direction': 'FORWARD', 'speed': 10}
-        input_data = usecases.DriveOperateInputData.from_dict(input_dict)
-        interactor.execute(input_data)
-        assert len(shadow_gateway.update.call_args_list) == 1
-
-    def test_no_operation(self, drive_context, motor_gateway, shadow_gateway):
-        input_data = usecases.DriveOperateInputData.from_dict({})
-
-        interactor = usecases.DriveOperateInteractor(
-            drive_context, motor_gateway, shadow_gateway)
-        with pytest.raises(exceptions.DriveOperationError):
-            interactor.execute(input_data)
